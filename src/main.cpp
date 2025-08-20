6f56ee9bab2df02a94823a196f4b2e71a97370a8
frederich <frederic.hallein@outlook.com>
2025-08-20 12:42:40 +0200

    fixed glfw and glad and have now working window

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
 #include "pch.hpp"
 
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GL_TRUE);
+static void processInput(GLFWwindow *window) {
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
 
-// static void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-//     glViewport(0, 0, width, height);
-// }
+static void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
 
 int main() {
     printSystemInfo();
@@ -15,13 +15,6 @@ int main() {
     GLFWwindow* window;
     if (!glfwInit())
         exit(EXIT_FAILURE);
-
-    // cout << "default shader lang: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << endl;
-
-    // select opengl version
-    // int major, minor, rev;
-    // glfwGetVersion(&major, &minor, &rev);
-    // cout << "glfw major.minor " << major << "." << minor << "." << rev << endl;
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
@@ -31,30 +24,34 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+    window = glfwCreateWindow(1080, 720, "ECS", NULL, NULL);
     if (!window) {
         LOG_ERROR("Failed to create GLFW window");
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
     glfwMakeContextCurrent(window);
-    // glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         LOG_ERROR("Failed to initialize GLAD");
         exit(EXIT_FAILURE);
     }
 
-    // LOG_INFO(glGetString(GL_SHADING_LANGUAGE_VERSION));
+    // select opengl version
+    int major, minor, rev;
+    glfwGetVersion(&major, &minor, &rev);
+    LOG_DEBUG("GLFW Version: ", major, '.' , minor, rev);
+    LOG_DEBUG("GLSL Version: ", glGetString(GL_SHADING_LANGUAGE_VERSION));
 
-    glfwSetKeyCallback(window, key_callback);
     while (!glfwWindowShouldClose(window)) {
-        float ratio;
-        int width, height;
-        glfwGetFramebufferSize(window, &width, &height);
-        // ratio = width / (float) height;
-        // glViewport(0, 0, width, height);
-        // glClear(GL_COLOR_BUFFER_BIT);
+        // input
+        processInput(window);
+
+        // render
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
